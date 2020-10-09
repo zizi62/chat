f70ff84177607834631a72243d72c802c50b39f4
Zoia <zizi2405@gmail.com>
2020-10-09 19:13:15 -0400

    add new message

--- src/app.ts
+++ src/app.ts
@@ -11,15 +11,30 @@ const app = express()
 const server = http.createServer(app)
 const io = socketIo(server)
 
+
+
+
+const messages = [{ message: "Hello TOM", id: "jhffhddf", user: { id: "nvnvn", name: "TOM" } },
+{ message: "Hello BOB", id: "jhfgghddf", user: { id: "dddvn", name: "BOB" } }, { message: "Hello NIMM", id: "jhfgddf", user: { id: "dddvn", name: "TIM" } }]
+
+
 app.get('/', (req, res) => {
-  res.send('HELLO SERVER');
+    res.send(messages);
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+io.on('connection', (socketChannel) => {
+
+    socketChannel.on("client-message-sent", (message: string) => {
+        let messageItem = { message: message, id: 'ab' + new Date().getTime(), user: { id: "dddvn", name: "BOB" } }
+        socketChannel.emit('new-message-sent', messageItem)
+    })
+
+
+    socketChannel.emit('init-messages-published', messages)
 });
+
 
 const PORT = process.env.PORT || 3009
 server.listen(PORT, () => {
-  console.log('listening on *:3009');
+    console.log('listening on *:3009' + PORT);
 });
