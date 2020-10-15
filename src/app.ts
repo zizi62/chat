f921613f6a3713736dc9283c61bdb6c96c5af753
Zoia <zizi2405@gmail.com>
2020-10-15 12:22:03 -0400

    add new

--- src/app.ts
+++ src/app.ts
@@ -12,25 +12,34 @@ const server = http.createServer(app)
 const io = socketIo(server)
 
 
-
-
 const messages = [{ message: "Hello TOM", id: "jhffhddf", user: { id: "nvnvn", name: "TOM" } },
 { message: "Hello BOB", id: "jhfgghddf", user: { id: "dddvn", name: "BOB" } }, { message: "Hello NIMM", id: "jhfgddf", user: { id: "dddvn", name: "TIM" } }]
 
+const users = new Map()
 
 app.get('/', (req, res) => {
     res.send(messages);
 });
 
 io.on('connection', (socketChannel) => {
+    users.set(socketChannel, { id: new Date().getTime().toString(), name: "anonim" })
+
+    io.on('disconnect', ()=>{
+        users.delete(socketChannel)
+    })
+    socketChannel.on('client-name-sent', (name:string)=>{
+        const user = users.get(socketChannel)
+        user.name = name;
+    })
+
     socketChannel.on("client-message-sent", (message: string) => {
-        let messageItem = { message: message, id: 'ab' + new Date().getTime(), user: { id: "dddvn", name: "BOB" } }
+        const user = users.get(socketChannel)
+        let messageItem = { message: message, id: new Date().getTime().toString(), user: { id: user.id, name: user.name } }
         messages.push(messageItem)
         socketChannel.emit('new-message-sent', messageItem)
     })
     socketChannel.emit('init-messages-published', messages)
 
-    console.log(messages)
 });
 
 
